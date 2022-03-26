11612cf45c066dfe88be719ebda05d949b4cf74a
Maximilian Burszley <mburszley@gmail.com>
2022-03-26 12:16:02 -0400

    session 02

--- hello/src/hello/__init__.py
+++ hello/src/hello/__init__.py
@@ -1,1 +1,19 @@
 __version__ = '0.1.0'
+
+
+class Button:
+    reserved: int | None
+
+    def __init__(self):
+        self.text = "some value"
+        self.width = 10
+        self.height = 10
+        self.background_color = "white"
+        self.foreground_color = "black"
+
+        self.reserved = None
+
+
+my_btn = Button()
+
+my_btn.reserved = "string"
